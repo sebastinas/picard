87d5c3d27b65e5879f6615925566485fe7f9ec1d
Laurent Monin <zas@norz.org>
2013-01-16 19:21:47 +0100

    bytestohuman code cleanup

--- picard/util/bytestohuman.py
+++ picard/util/bytestohuman.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 #
+# Picard, the next-generation MusicBrainz tagger
 # Copyright (C) 2013 Laurent Monin
 #
 # This program is free software; you can redistribute it and/or
@@ -16,20 +17,11 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-# hack to make doctest and gettext work together... :(
-try:
-    _("")
-except NameError:
-    def _(a_string):
-        return a_string
-except TypeError:
-    # during doctesting, _ might mean something else
-    pass
-
 class BytesToHuman(object):
     """
     Helper class to convert bytes to human-readable form
-    It supports i18n through gettext and both decimal and binary units
+    It supports i18n through gettext, decimal and binary units and abbreviated
+    notation or full text notation.
 
     >>> b = BytesToHuman()
     >>> n = 1572864
@@ -37,17 +29,6 @@ class BytesToHuman(object):
     ['1.5 MiB', '1.6 MB', '1.5 mebibytes', '1.6 megabytes']
     """
     def __init__(self, number=0, multiple=1000):
-        """
-        Inits BytesToHuman
-
-        >>> '1000 bytes = ' + str(BytesToHuman(1000))
-        '1000 bytes = 1 kB'
-        >>> str(BytesToHuman(0))
-        '0 B'
-        >>> b = BytesToHuman()
-        >>> [b.binary(4568979874), b.decimal(10**18)]
-        ['4.3 GiB', '1000 PB']
-        """
         self.number = number
         self.multiple = multiple
         self.__bytes_strings_i18n()
@@ -110,17 +91,17 @@ class BytesToHuman(object):
             N_('%d PB'): ungettext('%d petabyte', '%d petabytes', 0),
             N_('%d PiB'): ungettext('%d pebibyte', '%d pebibytes', 0),
 
-            N_('%0.1f B'): ungettext('%d byte', '%d bytes', 0), # no 0.1 byte ;)
-            N_('%0.1f kB'): ungettext('%0.1f kilobyte', '%0.1f kilobytes', 0),
-            N_('%0.1f KiB'): ungettext('%0.1f kibibyte', '%0.1f kibibytes', 0),
-            N_('%0.1f MB'): ungettext('%0.1f megabyte', '%0.1f megabytes', 0),
-            N_('%0.1f MiB'): ungettext('%0.1f mebibyte', '%0.1f mebibytes', 0),
-            N_('%0.1f GB'): ungettext('%0.1f gigabyte', '%0.1f gigabytes', 0),
-            N_('%0.1f GiB'): ungettext('%0.1f gibibyte', '%0.1f gibibytes', 0),
-            N_('%0.1f TB'): ungettext('%0.1f terabyte', '%0.1f terabytes', 0),
-            N_('%0.1f TiB'): ungettext('%0.1f tebibyte', '%0.1f tebibytes', 0),
-            N_('%0.1f PB'): ungettext('%0.1f petabyte', '%0.1f petabytes', 0),
-            N_('%0.1f PiB'): ungettext('%0.1f pebibyte', '%0.1f pebibytes', 0),
+            N_('%0.1f B'): N_('%d bytes'), # no 0.1 byte ;)
+            N_('%0.1f kB'): N_('%0.1f kilobytes'),
+            N_('%0.1f KiB'): N_('%0.1f kibibytes'),
+            N_('%0.1f MB'): N_('%0.1f megabytes'),
+            N_('%0.1f MiB'): N_('%0.1f mebibytes'),
+            N_('%0.1f GB'): N_('%0.1f gigabytes'),
+            N_('%0.1f GiB'): N_('%0.1f gibibytes'),
+            N_('%0.1f TB'): N_('%0.1f terabytes'),
+            N_('%0.1f TiB'): N_('%0.1f tebibytes'),
+            N_('%0.1f PB'): N_('%0.1f petabytes'),
+            N_('%0.1f PiB'): N_('%0.1f pebibytes'),
         }
         del ungettext
         del N_
@@ -131,17 +112,27 @@ class BytesToHuman(object):
         depending on `short` boolean value
         """
         n = int(number)
-        key = '%d' if number == n else '%0.1f'
-        key += ' ' + unit
-        if short:
-            return _(key) % number
+        if n == number:
+            key = '%d ' + unit
+            if short:
+                return _(key) % number
+            else:
+                return ungettext(self.BYTES_STRINGS[key][0],
+                                 self.BYTES_STRINGS[key][1], n) % n
         else:
-            return ungettext(self.BYTES_STRINGS[key][0],
-                             self.BYTES_STRINGS[key][1], number) % number
+            # float numbers always displayed as plural
+            # according to http://www.gnu.org/savannah-checkouts/gnu/gettext/manual/html_node/Plural-forms.html
+            key = '%0.1f ' + unit
+            if short:
+                return _(key) % number
+            else:
+                return _(self.BYTES_STRINGS[key]) % number
 
     @staticmethod
     def calc_unit(number, multiple=1000):
         """
+        Calculate rounded number of multiple * bytes, finding best unit
+
         >>> BytesToHuman.calc_unit(12456, 1024)
         (12.2, 'KiB')
         >>> BytesToHuman.calc_unit(-12456, 1000)
@@ -177,40 +168,11 @@ class BytesToHuman(object):
     def short_string(self, number, multiple=1000):
         """
         Returns short human-readable string for `number` bytes
-
-        >>> [BytesToHuman().short_string(15000), BytesToHuman().short_string(15000, 1024)]
-        ['15 kB', '14.6 KiB']
         """
         return self.textual(*self.calc_unit(number, multiple), short=True)
 
     def long_string(self, number, multiple=1000):
         """
         Returns long human-readable string for `number` bytes
-
-        >>> [BytesToHuman().long_string(15000), BytesToHuman().long_string(15000, 1024)]
-        ['15 kilobytes', '14.6 kibibytes']
         """
         return self.textual(*self.calc_unit(number, multiple), short=False)
-
-
-if __name__ == "__main__":
-    try:
-        import __builtin__
-    except ImportError:
-        import builtins as __builtin__ #Python 3.0
-
-    try:
-        a = _('')
-    except NameError:
-        __builtin__.__dict__['_'] = lambda a: a
-
-    try:
-        a = ungettext('', '', 0)
-    except NameError:
-        def ungettext(a, b, c):
-            if c == 1: return a
-            return b
-        __builtin__.__dict__['ungettext'] = ungettext
-
-    import doctest
-    doctest.testmod()
