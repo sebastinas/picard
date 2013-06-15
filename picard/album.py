b43eaad0f0cc0a8a058f12c0e52009f1cfd42d78
Laurent Monin <zas@norz.org>
2013-06-15 15:39:46 +0200

    Make modified releases a bit more obvious using a specific icon.

--- picard/album.py
+++ picard/album.py
@@ -420,6 +420,14 @@ class Album(DataObject, Item):
         else:
             return True
 
+    def is_modified(self):
+        if self.tracks:
+            for track in self.tracks:
+                for file in track.linked_files:
+                    if not file.is_saved():
+                        return True
+        return False
+
     def get_num_unsaved_files(self):
         count = 0
         for track in self.tracks:
