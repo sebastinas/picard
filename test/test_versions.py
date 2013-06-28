38d7261cbd9a022f30c37f18334503d118f6507d
Laurent Monin <zas@norz.org>
2013-06-28 15:28:10 +0200

    Add tests for version conversion (tuple<->string)

---
+++ test/test_versions.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from picard import version_to_string, version_from_string, VersionError
+
+class VersionsTest(unittest.TestCase):
+
+    def test_version_to_string_1(self):
+        t = [(0, 0, 1, 'dev', 1), '0.0.1dev1']
+        self.failUnlessEqual(version_to_string(t[0]), t[1])
+        self.failUnlessEqual(t[0], version_from_string(t[1]))
+
+    def test_version_to_string_2(self):
+        t = [(1, 1, 0, 'final', 0), '1.1']
+        self.failUnlessEqual(version_to_string(t[0]), t[1])
+        self.failUnlessEqual(t[0], version_from_string(t[1]))
+
+    def test_version_to_string_3(self):
+        t = [(1, 1, 0, 'dev', 0), '1.1.0dev0']
+        self.failUnlessEqual(version_to_string(t[0]), t[1])
+        self.failUnlessEqual(t[0], version_from_string(t[1]))
+
+    def test_version_to_string_4(self):
+        t = [(1, 0, 2, 'final', 0), '1.0.2']
+        self.failUnlessEqual(version_to_string(t[0]), t[1])
+        self.failUnlessEqual(t[0], version_from_string(t[1]))
+
+    def test_version_to_string_5(self):
+        t = [(10, 10, 10, 'dev', 10), '10.10.10dev10']
+        self.failUnlessEqual(version_to_string(t[0]), t[1])
+        self.failUnlessEqual(t[0], version_from_string(t[1]))
+
+    def test_version_to_string_6(self):
+        self.assertRaises(VersionError, version_from_string, 'abc')
+        self.assertRaises(VersionError, version_from_string, '')
+
+    def test_version_to_string_7(self):
+        self.assertRaises(TypeError, version_to_string, ('1', 0, 2, 'final', 0))
+        self.assertRaises(IndexError, version_to_string, (1, 0))
+        self.assertRaises(TypeError, version_from_string, 1)
+
